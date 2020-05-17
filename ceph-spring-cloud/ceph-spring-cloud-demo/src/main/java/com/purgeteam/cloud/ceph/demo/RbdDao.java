be2eaabd1bcddf48abbc4e844226b6a56f79215b
Purgeyao <zhaoy@deepblueai.com>
2020-05-17 09:21:18 +0800

    feat(spirng-cloud-purgeteam):整理整体pom结构。

--- ceph-spring-cloud/ceph-spring-cloud-demo/src/main/java/com/purgeteam/cloud/ceph/demo/RbdDao.java
+++ ceph-spring-cloud/ceph-spring-cloud-demo/src/main/java/com/purgeteam/cloud/ceph/demo/RbdDao.java
@@ -2,7 +2,6 @@ package com.purgeteam.cloud.ceph.demo;
 
 import com.ceph.rados.IoCTX;
 import com.ceph.rados.Rados;
-import com.ceph.rados.exceptions.RadosException;
 import com.ceph.rbd.Rbd;
 import com.ceph.rbd.RbdException;
 import com.ceph.rbd.RbdImage;
@@ -17,10 +16,11 @@ public class RbdDao {
     private static Rados rados;
     private static IoCTX ioctx;
     private static Rbd rbd;
+
     /**
      * 连接上ceph环境
      */
-    public static void connectCeph(){
+    public static void connectCeph() {
         try {
             //利用参数连接Ceph
             rados = new Rados("liuyz");
@@ -38,13 +38,14 @@ public class RbdDao {
 
     /**
      * 返回所有的image,并展示其详细信息
-     * @return
+     *
+     * @return image list
      */
-    public static List<String> imageList(){
-        List<String> imageList=null;
+    public static List<String> imageList() {
+        List<String> imageList = null;
         try {
             imageList = Arrays.asList(rbd.list());
-            for(String s:imageList){
+            for (String s : imageList) {
                 showDetailOfImage(s);
             }
         } catch (RbdException e) {
@@ -56,17 +57,18 @@ public class RbdDao {
 
     /**
      * 显示image的详细信息
-     * @param imageName
+     *
+     * @param imageName 镜像名称
      */
-    public static void showDetailOfImage(String imageName){
+    public static void showDetailOfImage(String imageName) {
         RbdImage image;
         try {
             image = rbd.open(imageName);
             RbdImageInfo info = image.stat();
             System.out.println("=================================================================");
-            System.out.println("imageName:    "+imageName);
-            System.out.println("imageSize:    "+info.size);
-            System.out.println("order:   "+info.order);
+            System.out.println("imageName:    " + imageName);
+            System.out.println("imageSize:    " + info.size);
+            System.out.println("order:   " + info.order);
             rbd.close(image);
         } catch (RbdException e) {
             // TODO Auto-generated catch block
@@ -77,15 +79,16 @@ public class RbdDao {
 
     /**
      * 以格式1创建image
+     *
      * @param imageName 名称
-     * @param imageSize　大小
+     * @param imageSize 　大小
      */
-    public static void createRbd_format1(String imageName, long imageSize){
+    public static void createRbd_format1(String imageName, long imageSize) {
         try {
             rbd.create(imageName, imageSize);
             RbdImage image = rbd.open(imageName);
             boolean oldFormat = image.isOldFormat();
-            System.out.println("imageFormat:==========================="+oldFormat);
+            System.out.println("imageFormat:===========================" + oldFormat);
 
             rbd.close(image);
         } catch (RbdException e) {
@@ -95,18 +98,19 @@ public class RbdDao {
 
     /**
      * 以格式2创建image，ceph 仅支持克隆 format 2 映像（即用 rbd create –format 2 创建的），而且内核 rbd 模块还不支持。
-     　　所以现在你 只能用 QEMU/KVM 或 librbd直接访问克隆品
+     * 　　所以现在你 只能用 QEMU/KVM 或 librbd直接访问克隆品
+     *
      * @param imageName 名称
-     * @param imageSize　大小
+     * @param imageSize 　大小
      */
-    public static void createRbd_format2(String imageName, long imageSize){
+    public static void createRbd_format2(String imageName, long imageSize) {
         try {
-            int features = (1<<0);
-            System.out.println("features=============="+features);
-            rbd.create(imageName, imageSize,features, 0);
+            int features = (1 << 0);
+            System.out.println("features==============" + features);
+            rbd.create(imageName, imageSize, features, 0);
             RbdImage image = rbd.open(imageName);
             boolean oldFormat = image.isOldFormat();
-            System.out.println("imageFormat:==========================="+oldFormat);
+            System.out.println("imageFormat:===========================" + oldFormat);
             rbd.close(image);
             image.flatten();
         } catch (RbdException e) {
@@ -116,15 +120,16 @@ public class RbdDao {
 
     /**
      * 方法创建一个image并对重设置大小为初始化大小的2倍
-     * @param imageName
+     *
+     * @param imageName 镜像名称
      */
-    public static void resizeImage(String imageName){
+    public static void resizeImage(String imageName) {
         long initialSize = 10485760;
         long newSize = initialSize * 2;
         try {
-            int features = (1<<0);
-            System.out.println("features=============="+features);
-            rbd.create(imageName, initialSize,features, 0);
+            int features = (1 << 0);
+            System.out.println("features==============" + features);
+            rbd.create(imageName, initialSize, features, 0);
             RbdImage image = rbd.open(imageName);
             image.resize(newSize);
             rbd.close(image);
@@ -135,10 +140,11 @@ public class RbdDao {
 
     /**
      * 创建映像的快照
+     *
      * @param imageName 映像名称
-     * @param snapName 快照名称
+     * @param snapName  快照名称
      */
-    public static void createSnap(String imageName,String snapName){
+    public static void createSnap(String imageName, String snapName) {
         try {
             RbdImage image = rbd.open(imageName);
             //创建快照
@@ -147,9 +153,9 @@ public class RbdDao {
             image.snapProtect(snapName);
             //返回一个image的所有快照
             List<RbdSnapInfo> snaps = image.snapList();
-            for(RbdSnapInfo rbds:snaps){
-                System.out.println("快照名称："+rbds.name);
-                System.out.println("快照大小："+rbds.size);
+            for (RbdSnapInfo rbds : snaps) {
+                System.out.println("快照名称：" + rbds.name);
+                System.out.println("快照大小：" + rbds.size);
             }
         } catch (RbdException e) {
             // TODO Auto-generated catch block
@@ -159,12 +165,13 @@ public class RbdDao {
 
     /**
      * 通过快照克隆出新的image
+     *
      * @param parentImageName 快照对应的image名称
-     * @param snapName 快照的名称
-     * @param newImageName 生成的新的image的名称
+     * @param snapName        快照的名称
+     * @param newImageName    生成的新的image的名称
      */
-    public static void copySnapToNewImage(String parentImageName,String snapName,String newImageName){
-        int features = (1<<0);
+    public static void copySnapToNewImage(String parentImageName, String snapName, String newImageName) {
+        int features = (1 << 0);
         try {
             rbd.clone(parentImageName, snapName, ioctx, newImageName, features, 0);
         } catch (RbdException e) {
@@ -175,10 +182,11 @@ public class RbdDao {
 
     /**
      * 删除某个image的名叫 snapName的快照,需要注意的是要删除快照，必须保证快照没有copy的子image，否则会删除失败。
-     * @param imageName
-     * @param snapName
+     *
+     * @param imageName 镜像名称
+     * @param snapName snapName
      */
-    public static void deleteSnap(String imageName,String snapName){
+    public static void deleteSnap(String imageName, String snapName) {
         try {
             RbdImage image = rbd.open(imageName);
             image.snapUnprotect(snapName);
@@ -191,11 +199,10 @@ public class RbdDao {
 
     /**
      * 删除某一个image
-     * @param r
-     * @param io
-     * @param imageName
-     * @throws RadosException
-     * @throws RbdException
+     *
+     * @param r Rados
+     * @param io IoCTX
+     * @param imageName 镜像名称
      */
     public static void cleanupImage(Rados r, IoCTX io, String imageName) {
         try {
@@ -213,7 +220,7 @@ public class RbdDao {
     }
 
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         connectCeph();
         //createRbd_format1("mysql-hzb-2",10737418240l);
         //createRbd_format2("imageformat2",10485760);
